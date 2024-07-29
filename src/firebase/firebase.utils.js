2e7e8e53a7ae6a1cd41c1bd17069983af330f45a
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2024-07-29 13:35:02 +0000

    chore: Update project structure and styles for shop page

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const Config = {
   measurementId: "G-VWE9YZERCB",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // Create a reference to the user in the database
+  const snapShot = await userRef.get(); // Get the snapshot of the user
+
+  if (!snapShot.exists) {
+    // If the user does not exist in the database, create a new user
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      // Try to create a new user in the database
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      // If there is an error, log the error
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(Config);
 
 export const auth = firebase.auth();
