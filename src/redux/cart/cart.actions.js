81fc6108a487f91fc5b35ec7954406a9df7b4270
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2024-08-01 13:26:24 +0000

    chore: Refactor checkout-item component to use Redux actions and connect to Redux store

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item,
 }); // Change this line
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+}); // Change this line
+
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
