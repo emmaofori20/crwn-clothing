ad3b5b7e3e52e2095448c8384ab5ed994f9fd7ab
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2024-08-05 09:43:47 +0000

    chore: Remove unused import in shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useMatch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
