839651498a5f13259c0fdaacaf4698623009a3fb
Emmanuel Ofori <emmanuelofori2638@gmail.com>
2024-08-05 09:54:05 +0000

    chore: Export store and persistor as separate object in redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,4 +10,6 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+const exportedObject = { store, persistor };
+
+export default exportedObject;
